4088246fd6a0d806448c7667cd38cae248613ee1
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-10-13 16:09:01 +0500

    Added env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
 import logger from "./startup/logger";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  logger.info("Server is listening on port 3000...");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  logger.info(`Server is listening on port ${port}...`);
 });
